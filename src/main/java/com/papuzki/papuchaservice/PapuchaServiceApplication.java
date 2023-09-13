b08bcc09f28c42235762184c69b7dfd9460c6af3
reevee <mminun@gmail.com>
2023-09-13 20:36:15 +0200

    init

---
+++ src/main/java/com/papuzki/papuchaservice/PapuchaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.papuzki.papuchaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapuchaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PapuchaServiceApplication.class, args);
+	}
+
+}
