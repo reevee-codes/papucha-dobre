3a368657c23e7edf5e17d74e1b0e978ee524bd2c
Reevee <mminun@gmail.com>
2023-09-14 22:07:59 +0200

    dziala XD

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS papugi (
     id INT8 NOT NULL,
     name VARCHAR(255),
+    color VARCHAR(255),
     PRIMARY KEY (id)
 );
 
-INSERT INTO papugi (id, name) VALUES (1, 'Sebusia');
-INSERT INTO papugi (id, name) VALUES (2, 'Marcel');
+INSERT INTO papugi (id, name, color) VALUES (1, 'Sebusia', 'blue');
+INSERT INTO papugi (id, name, color) VALUES (2, 'Marcel', 'green');
